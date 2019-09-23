7783fb71bdc4cc8f4a1ddf1c6012bc791276d127
Thorsten Stanitzok <thorsten.stanitzok@googlemail.com>
2019-09-23 12:54:02 +0200

    bound welcome message to set channel config

--- src/bot/events/message.ts
+++ src/bot/events/message.ts
@@ -2,54 +2,49 @@ import * as Discord from "discord.js";
 import { Client } from "../..";
 
 module.exports = (client: Client, message: Discord.Message): void => {
-  // Ignore all bots
-  if (message.author.bot) return;
+    // Ignore all bots
+    if (message.author.bot) return;
 
-  if (!message.isMentioned(client.users.filter(user => user.id === client.user.id).first())) {
-    return;
-  }
-
-  // Ignore messages not starting with the prefix (in config.json)
-  if (message.content.indexOf(client.bot.config.prefix) < 0) return;
-
-  // Our standard argument/command name definition.
-  const args = message.content.trim().split(/ +/g) || [];
-
-  console.log("message-event:", args);
-
-  if (!args || args.length <= 0) {
-    return;
-  }
-
-  let command = "";
-
-  args.forEach(argument => {
-    if (argument.startsWith(client.bot.config.prefix)) {
-      command = argument.slice(client.bot.config.prefix.length);
+    // Ignore messages not starting with bot mention or the prefix (in config.json)
+    if (!message.isMentioned(client.users.filter(user => user.id === client.user.id).first()) && message.content.indexOf(client.bot.config.prefix) < 0) {
+        return;
     }
-  });
 
-  if (command) {
-    command = command.toLowerCase();
-  } else {
-    return;
-  }
-  client.bot.logger.debug(`looking for command: <${command}> in:`);
-  client.bot.commands.forEach((v, k) => {
-    client.bot.logger.debug(`${k}: ${v}`);
-  });
-  // Grab the command data from the client.commands Enmap
-  const cmd = client.bot.commands.get(command);
+    // Our standard argument/command name definition.
+    const args = message.content.trim().split(/ +/g) || [];
 
-  // If that command doesn't exist, silently exit and do nothing
+    console.log("message-event:", args);
 
-  if (!cmd) {
-    client.bot.logger.error(`Command <${command} ${typeof cmd}> not found...`);
-    return;
-  } else {
-    client.bot.logger.debug(`Command <${command} ${typeof cmd}> found...`);
-  }
+    if (!args || args.length <= 0) {
+        return;
+    }
 
-  // Run the command
-  cmd.run(client, message, args);
+    let command = "";
+
+    args.forEach(argument => {
+        if (argument.startsWith(client.bot.config.prefix)) {
+            command = argument.slice(client.bot.config.prefix.length);
+        }
+    });
+
+    if (command) {
+        command = command.toLowerCase();
+    } else {
+        return;
+    }
+
+    // Grab the command data from the client.commands Enmap
+    const cmd = client.bot.commands.get(command);
+
+    // If that command doesn't exist, silently exit and do nothing
+
+    if (!cmd) {
+        client.bot.logger.error(`Command <${command} ${typeof cmd}> not found...`);
+        return;
+    } else {
+        client.bot.logger.debug(`Command <${command} ${typeof cmd}> found...`);
+    }
+
+    // Run the command
+    cmd.run(client, message, args);
 };
