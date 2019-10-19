afc47963ec82d328f73de53c0badc7189a4e706b
Thorsten Stanitzok <thorsten.stanitzok@googlemail.com>
2019-10-19 10:41:41 +0200

    Fixed C&P error for discord events.

--- src/bot/core/DiscordEvents.ts
+++ src/bot/core/DiscordEvents.ts
@@ -1,11 +1,9 @@
-import { DiscordEvent } from "../api/command";
+import { DiscordEvent } from '../api/command';
 
 export class DiscordEvents {
-
   static readonly READY: DiscordEvent = { eventName: 'ready', callback: 'onReady' };
   static readonly DISCONNECT: DiscordEvent = { eventName: 'ready', callback: 'onDisconnect' };
   static readonly RECONNECTING: DiscordEvent = { eventName: 'ready', callback: 'onReconnecting' };
-
 
   static readonly MESSAGE: DiscordEvent = { eventName: 'message', callback: 'onMessage' };
 
@@ -14,9 +12,8 @@ export class DiscordEvents {
 
   static readonly PRESENCE_UPDATE: DiscordEvent = { eventName: 'presenceUpdate', callback: 'onPresenceUpdate' };
 
-
   static readonly GUILD_MEMBER_ADD: DiscordEvent = { eventName: 'guildMemberAdd', callback: 'onGuildMemberAdd' };
-  static readonly GUILD_MEMBER_REMOVE: DiscordEvent = { eventName: 'guildMemberAdd', callback: 'onGuildMemberRemove' };
+  static readonly GUILD_MEMBER_REMOVE: DiscordEvent = { eventName: 'guildMemberRemove', callback: 'onGuildMemberRemove' };
 
   private constructor(private readonly event: string, public readonly callback: string) {
     //empty
