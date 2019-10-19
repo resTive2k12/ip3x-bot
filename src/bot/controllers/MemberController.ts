0e219312cbf797a47e540f74b67cd042f88d8f7f
Thorsten Stanitzok <thorsten.stanitzok@googlemail.com>
2019-10-19 10:43:04 +0200

    implemented more member functions
    added:
     - presence update
     - server join
     - server leave

--- src/bot/controllers/MemberController.ts
+++ src/bot/controllers/MemberController.ts
@@ -1,8 +1,7 @@
 import { Client } from '../api/client';
 import { AbstractController } from './AbstractController';
 import { DiscordEvents } from '../core/DiscordEvents';
-import * as Discord from "discord.js";
-import { stringify } from 'querystring';
+import * as Discord from 'discord.js';
 import { User } from '../api/storage';
 
 export class MemberController extends AbstractController {
@@ -19,7 +18,7 @@ export class MemberController extends AbstractController {
       const entry = await this.client.db.fetch(guild.id);
       if (!entry) return;
       if (!entry.users) entry.users = [];
-      const users = new Map(entry.users.map(e => [e.id, e]));
+      const users = MemberController.usersToMap(entry.users);
       console.debug(`Checking guild ${guild.name} [${guild.id}]...`);
       guild.members.forEach(member => {
         const userIsKnown = users.size > 0 && !!users.get(member.id);
@@ -30,8 +29,7 @@ export class MemberController extends AbstractController {
           console.debug(`\t- User ${member.nickname || member.user.username} [${member.user.id}] is present.`);
         }
       });
-      entry.users = Array.from(users.values());
-      console.log(entry.users);
+      entry.users = MemberController.usersToArray(users);
       this.client.db.update(entry);
     });
   }
@@ -40,23 +38,61 @@ export class MemberController extends AbstractController {
     const oldStatus = oldUser.presence.status;
     const newStatus = newUser.presence.status;
 
-    if (newStatus == "offline") {
+    if (newStatus == 'offline' || newStatus == 'online' || newStatus == 'idle') {
       this.client.db.fetch(newUser.guild.id).then(entry => {
         if (!entry.users) {
-          console.trace("Users should be present here...");
+          console.trace('Users should be present here...');
           return;
         }
-        const user = entry.users.get(newUser.id);
+        const users = MemberController.usersToMap(entry.users);
+        const user = users.get(newUser.id);
         if (!user) {
           console.trace(`User "${newUser.nickname || newUser.user.username}" [${newUser.id}] should be present here...`);
           return;
         }
         user.lastSeen = new Date();
-        entry.users.set(newUser.id, user);
+        users.set(newUser.id, user);
+        console.debug(`Updated users ${newUser.nickname || newUser.user.username} [${newUser.user.id}] presence from ${oldStatus} to ${newStatus}.`);
+        entry.users = MemberController.usersToArray(users);
         this.client.db.update(entry);
       });
     }
+  }
 
-    console.log(`${newUser.nickname || newUser.user.username} switch from ${oldStatus} to ${newStatus}`);
+  onGuildMemberAdd(newMember: Discord.GuildMember): void {
+    this.client.db.fetch(newMember.guild.id).then(entry => {
+      if (!entry) return;
+      if (!entry.users) entry.users = [];
+      const users = MemberController.usersToMap(entry.users);
+      users.set(newMember.user.id, { id: newMember.user.id, name: newMember.user.username, joinedAt: newMember.joinedAt, isBot: newMember.user.bot });
+      console.debug(`Added new user ${newMember.nickname || newMember.user.username} [${newMember.user.id}].`);
+      entry.users = MemberController.usersToArray(users);
+      this.client.db.update(entry);
+    });
+  }
+
+  onGuildMemberRemove(member: Discord.GuildMember): void {
+    this.client.db.fetch(member.guild.id).then(entry => {
+      if (!entry) return;
+      if (!entry.users) entry.users = [];
+      const users = MemberController.usersToMap(entry.users);
+      const user = users.get(member.user.id);
+      if (user) {
+        user.leftAt = new Date();
+        user.lastSeen = user.leftAt;
+        users.set(user.id, user);
+        entry.users = MemberController.usersToArray(users);
+        this.client.db.update(entry);
+        console.debug(`Terminated left user ${member.nickname || member.user.username} [${member.user.id}].`);
+      }
+    });
+  }
+
+  static usersToMap(users: User[]): Map<string, User> {
+    return new Map(users.map(e => [e.id, e]));
+  }
+
+  static usersToArray(users: Map<string, User>): User[] {
+    return Array.from(users.values());
   }
 }
