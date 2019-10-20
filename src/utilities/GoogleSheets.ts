5c4f7e50b3ba77dc1a323f0e7042af118e33a2a1
Thorsten Stanitzok <thorsten.stanitzok@googlemail.com>
2019-10-20 07:07:07 +0200

    Housekeeping

--- src/utilities/GoogleSheets.ts
+++ src/utilities/GoogleSheets.ts
@@ -1,17 +1,16 @@
-import { google } from "googleapis";
-import { JWT, CredentialBody } from "google-auth-library";
-import { BotConfig } from "../bot/api/botconfig";
+import { google } from 'googleapis';
+import { JWT, CredentialBody } from 'google-auth-library';
 
 export class GoogleSheets {
-    private static SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
+  private static SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 
-    public static async auth(credentials: CredentialBody): Promise<JWT> {
-        const auth = new google.auth.GoogleAuth({
-            //keyFile: "./config/ip3x-jwt.json",
-            credentials: credentials,
-            scopes: GoogleSheets.SCOPES
-        });
-        new google.auth.GoogleAuth({});
-        return auth.getClient() as Promise<JWT>;
-    }
+  public static async auth(credentials: CredentialBody): Promise<JWT> {
+    const auth = new google.auth.GoogleAuth({
+      //keyFile: "./config/ip3x-jwt.json",
+      credentials: credentials,
+      scopes: GoogleSheets.SCOPES
+    });
+    new google.auth.GoogleAuth({});
+    return auth.getClient() as Promise<JWT>;
+  }
 }
